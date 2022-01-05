015793d4b305c681be059d913cda702cb2625cf2
Fahad Ausaf <fahad.ausaf@icloud.com>
2022-01-05 12:42:50 +0000

    Zara Cleanup Functions

--- Adapters/Functions.py
+++
@@ -1,6 +0,0 @@
-
-def itemInList(list, item):
-    for i in range(len(list)):
-        if list[i] == item:
-            return True
-    return False
--- Adapters/Zara.py
+++ Adapters/Zara.py
@@ -1,46 +1,14 @@
 import json
-from Functions import *
 
 dataSource = "../Data/Zara/output.json"
 cleanDataSource = "../Data/Zara/clean_output.json"
-
 zaraProducts = json.load(open(dataSource))
 
-def displayData():
-    num = 0
-    for product in zaraProducts:
-        num += 1
-        print("ID: " + str(num))    
-        print("Name: " + product["name"])
-        print("Description: " + product["description"])
-        print("Category: " + product["category"])
-        
-        sub_category = ""
-        for cat in product["sub_category"].split(" | "):
-            if (sub_category != ""):
-                sub_category = sub_category + ", " + cat
-            else:
-                sub_category = cat
-        print("Sub-Category: " + sub_category)
-
-        color = product["color"].split(" | ")
-        color = color[0].split("Colour ")
-        try:
-            color = color[1]
-        except:
-            color = ""
-        print("Color: " + color)
-
-        images = []
-        for img in product["images"]:
-            images.append(img)
-
-        print(images)
-        
-        print("Price: " + product["price"])
-        print("SKU: " + product["sku"])
-        print("URL: " + product["url"])
-        print("")
+def itemInList(list, item):
+    for i in range(len(list)):
+        if list[i] == item:
+            return True
+    return False
 
 def getCategories():
     categories = []
